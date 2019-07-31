282be700c8a89083c0146f26da65b0c0123d67ee
Angelika <maciazekangelika@gmail.com>
2019-07-31 16:16:36 +0200

    add appbar, create first folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import AppBar from './AppBar'
+import AppBar from './containers/app-bar'
 import SideBar from './SideBar'
 import Dashboard from './Dashboard'
 
 function App() {
   return (
     <div className="App">
-   
+   <AppBar></AppBar>
     </div>
   )
 }
