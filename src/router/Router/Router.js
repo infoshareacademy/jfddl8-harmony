5aa4a79f5cbe948a38d7428354d1788ac349c3a9
Angelika <maciazekangelika@gmail.com>
2019-08-01 16:52:51 +0200

    create folders and files to menu

--- src/router/Router/Router.js
+++ src/router/Router/Router.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import FetchUsersContainer from '../../containers/FetchUsersContainer'
+import ContactListContainer from '../../containers/ContactsListContainer/ContactsListContainer'
+
 const Router = (props) => (
+  <BrowserRouter>
+    {props.children}
     <div>
-        Router
+      <Route path={'/fetch-users'} component={FetchUsersContainer} />
+      <Route path={'/contact-list'} component={ContactListContainer} />
     </div>
+  </BrowserRouter>
 )
 
-export default Router 
+export default Router
--- src/router/SideBar/SideBar.js
+++ src/router/SideBar/SideBar.js
@@ -1,8 +1,29 @@
 import React from 'react'
-const SideBar = (props) => (
-    <div>
-        SideBar
-    </div>
-)
+import Drawer from '@material-ui/core/Drawer'
+import SideBarItem from '../SideBar/SideBarItem'
 
-export default SideBar 
+
+const styles = {
+  innerDrawer: {
+    width: 200
+  }
+}
+
+class SideBar extends React.Component {
+  render() {
+    return (
+      <Drawer
+        open={this.props.isSideBarOpen}
+        onClose={this.props.toggleSideBar}
+      >
+        <div style={styles.innerDrawer}>
+          <SideBarItem to={'/fetch-users'} label={'Fetch Users'} />
+          <SideBarItem to={'/contact-list'} label={'ContactList'} />
+        </div>
+      </Drawer>
+    )
+  }
+
+}
+
+export default SideBar
