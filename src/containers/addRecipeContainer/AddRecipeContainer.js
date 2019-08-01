585800f2ec982c86ab428736e4549ae24ed95eb5
Beata Rudzka <beatarudzka89@gmail.com>
2019-08-01 21:27:31 +0200

    adding AddRecipeContainer

---
+++ src/containers/addRecipeContainer/AddRecipeContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class AddRecipeContainer extends React.Component {
+  state = {
+    recipes = null
+  }
+  render(){
+    const recipes = this.state.recipes && this.state.recipes.map(
+      recipe => ({
+        recipe.
+      })
+    )
+
+  {
+    return (
+      <div>
+        AddRecipeContainer
+      </div>
+    )
+  }
+}
+
+
+export default AddRecipeContainer
