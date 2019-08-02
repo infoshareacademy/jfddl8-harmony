ac7e5c3d1087aeace2fd40886b053f3e237392df
Beata Rudzka <beatarudzka89@gmail.com>
2019-08-02 16:08:44 +0200

    skeleton of addRecipeContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import AppBar from './containers/app-bar'
 import View from './containers/view'
+import AddRecipeContainer from './containers/addRecipeContainer'
 
 function App() {
   return (
     <div className="App">
-   <AppBar></AppBar>
-   <div>
-   <View/>
-
-   </div>
-
+      <AppBar />
+      <View />
+      <AddRecipeContainer/>
     </div>
   )
 }
--- src/containers/addRecipeContainer/AddRecipeContainer.js
+++ src/containers/addRecipeContainer/AddRecipeContainer.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
+
 class AddRecipeContainer extends React.Component {
   state = {
-    recipes = null
-  }
-  render(){
-    const recipes = this.state.recipes && this.state.recipes.map(
-      recipe => ({
-        recipe.
-      })
-    )
+    title: '',
+    ingredients: '',
+    description:'',
+    nutritiveValue: '',
 
-  {
+  }
+  render() {
     return (
       <div>
-        AddRecipeContainer
+        <h1>Dodaj swój przepis</h1>
+        <h3>Wpisz tytuł przepisu</h3>
+        <input type={'text'}/>
+        <br />
+        <h3>Składniki</h3>
+        <input type={'text'}/>
+        <br />
+        <h3>Przygotowanie</h3>
+        <input type={'text'}/>
+        <h3>Wartość energetyczna</h3>
+        <input type={'text'}/>
+        <br />
+        <button>Zapisz</button>
       </div>
     )
   }
 }
 
-
 export default AddRecipeContainer
+
