9bf13dab040098ca51052f64e41789a2df2be9c6
Beata Rudzka <beatarudzka89@gmail.com>
2019-08-06 16:13:47 +0200

    refactoring

--- src/containers/addRecipeContainer/recipesService.js
+++ src/containers/addRecipeContainer/recipesService.js
@@ -1,6 +1,16 @@
 
 const URL = 'https://jfddl8-harmonylublin.firebaseio.com/recipes'
 
+
+export const deleteRecipe = (recipeKey) => {
+  return fetch(
+     URL + '/' + recipeKey + '.json',
+  {
+    method: 'DELETE'
+  }
+  )
+}
+
 export const addRecipe = (newRecipe) => {
   return fetch(
     URL + '.json',
@@ -11,11 +21,3 @@ export const addRecipe = (newRecipe) => {
   )
 }
 
-export const deleteRecipe = (recipeKey) => {
-  return fetch(
-    URL + '/' + recipeKey + '.json',
-    {
-      method: 'DELETE'
-    }
-  )
-}
