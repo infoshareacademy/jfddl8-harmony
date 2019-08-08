4b8ccc7ec9c14e9d7c282b5bd511a0f6e79554e6
AdamWitzb <a.witzberg@gmail.com>
2019-08-09 00:23:17 +0200

    added favorite button instead of info icon in list-item

--- src/components/list-item/index.jsx
+++ src/components/list-item/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import IconButton from "@material-ui/core/IconButton";
-import InfoIcon from "@material-ui/icons/Info";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 const ListItem = props => {
   const recipes = props.recipes;
@@ -21,7 +21,7 @@ const ListItem = props => {
             subtitle={<span>{el.nutritiveValue} kcal</span>}
             actionIcon={
               <IconButton>
-                <InfoIcon />
+                <FavoriteBorder />
               </IconButton>
             }
           />
