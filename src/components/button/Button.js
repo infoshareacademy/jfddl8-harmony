e97071217f5950eb86e1a8799c177c94f1cdc522
Beata Rudzka <beatarudzka89@gmail.com>
2019-08-11 16:22:15 +0200

    changing olor of button for default

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -5,7 +5,7 @@ import { Button as MuiButton } from '@material-ui/core/'
 const Button = (props) => {
   return (
     <MuiButton
-      color="primary" 
+      color="default" 
       fullWidth
       onClick={props.onClick}
       variant="contained"
