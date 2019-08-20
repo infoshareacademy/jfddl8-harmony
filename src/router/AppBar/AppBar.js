ada61abbcba5ca6d022a3ef1b66a01fbe347aa51
Angelika Maciazek <maciazekangelika@gmail.com>
2019-08-20 14:34:49 +0200

    share working

--- src/router/AppBar/AppBar.js
+++ src/router/AppBar/AppBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
 import { AppBar as AppBarStyled, Logo as IconLogo } from './AppBar.styled'
+import { FacebookShareButton, FacebookIcon } from 'react-share'
 import LogoPic from '../../img/logopoziomprzezroczystetlo.png'
 
 
@@ -9,8 +10,11 @@ const AppBar = (props) => (
     <AppBarStyled position="static" >
       <Toolbar >
         <IconLogo>
-          <img src={LogoPic} />
+          <img src={LogoPic} alt={''} />
         </IconLogo>
+        <FacebookShareButton url={'http://app.move-your-fit.jfddl8.is-academy.pl/'}>
+          <FacebookIcon />
+        </FacebookShareButton>
       </Toolbar>
     </AppBarStyled>
   </div>
