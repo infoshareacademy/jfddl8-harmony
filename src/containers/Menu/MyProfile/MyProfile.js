ec54e1409098e9ef95c6c60f6f57a7f39549e9ef
Beata Rudzka <beatarudzka89@gmail.com>
2019-08-20 20:59:12 +0200

    creating service and mapObjectToArray

--- src/containers/Menu/MyProfile/MyProfile.js
+++ src/containers/Menu/MyProfile/MyProfile.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 
-const MyProfile = (props) => (
-    <div>
-        MyProfile
-    </div>
-)
+class MyProfile extends React.Component {
+    state = {}
+
+    componentDidMount() {
+        fetch('https://jfddl8-harmonylublin.firebaseio.com/users')
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    avatarUrl: data.avatar_url,
+                    name: data.name,
+                    username: data.login,
+                    location: data.location,
+                })
+            })
+    }
+    render() {
+        return (
+            <div >
+                <img src={this.state.avatarUrl} />
+                <p>@{this.state.username}</p>
+                <p>{this.state.name} in {this.state.location}</p>
+            </div >
+        )
+    }
+}
 
 export default MyProfile 
